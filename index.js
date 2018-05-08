9070ce299c2deba5c1398f6d59625f72724990d7
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-08 00:21:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  return document.getElementById('grand-node').querySelector('div div div div')
+  return document.getElementById('grand-node').querySelector('div')
 
 }
