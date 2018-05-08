3f2e7467ea84bec5cdcc14025f8725b14fc0d2de
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-08 00:10:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementByClassName('.ranked-list').querySelectorAll('li')
+  const lis = document.querySelectorAll('.ranked-list li')
 
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + n).toString()
